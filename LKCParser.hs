dbbb397fb7e50ac2851020bced18eecf9b25a3d0
mzanella <zanna0150@gmail.com>
2017-01-02 12:15:42 +0100

    added comment and reordered functions

--- LKCParser.hs
+++ LKCParser.hs
@@ -2,8 +2,10 @@ import Control.Monad
 import Control.Applicative
 import Data.Char
 
--- let x=2 and y=4 in x+y*2 end
--- "letrec fact=lambda(n) if eq(n,1) then 1 else n* fact (n-1) and x=cons(1, cons( 2,null)) and f = lambda (l,g) if eq(l, null) then null else cons(g (head(l)), f (g, tail (l))) in x+y*2 end"
+-- eval "let x=2 and y=4 in x+y*2 end"
+-- eval "letrec fact=lambda(n) if eq(n,1) then 1 else n* fact (n-1) and x=cons(1, cons( 2,null)) and f = lambda (l,g) if eq(l, null) then null else cons(g (head(l)), f (g, tail (l))) in f(x,fact) end"
+
+-- tree data type to represent value parsed from the given grammar
 data LKC = VAR String | NUM Int | NULL | ADD LKC LKC |
            SUB LKC LKC | MULT LKC LKC | DIV LKC LKC |
            EQ LKC LKC | LEQ LKC LKC | H LKC | T LKC | CONS LKC LKC |
@@ -11,17 +13,21 @@ data LKC = VAR String | NUM Int | NULL | ADD LKC LKC |
            LET LKC [(LKC,LKC)] | LETREC LKC [(LKC, LKC)]
            deriving(Show, Eq)
 
+-- parser type
 newtype Parser a = P(String -> [(a, String)])
 
+-- apply a parser to a given data
 parse :: Parser a -> String -> [(a, String)]
 parse (P parser) input = parser input
 
+-- parser is a functor -> defined because of monad is needed
 instance Functor Parser where
     -- (a -> b) -> Parser a -> Parser b
     fmap f parser = P(\input -> case parse parser input of 
                                     [] -> []
                                     [(v,other)] -> [(f v,other)])
 
+-- parser is an applicative -> defined because of monad is needed
 instance Applicative Parser where
     -- a -> Parser a
     pure v = P(\input -> [(v,input)])
@@ -31,6 +37,7 @@ instance Applicative Parser where
                                         [] -> []
                                         [(f,other)] -> parse (fmap f parserx) other)
 
+-- parser is a monad
 instance Monad Parser where
     -- a -> Parser a
     return = pure
@@ -40,6 +47,7 @@ instance Monad Parser where
                                 [] -> []
                                 [(v,other)] -> parse (f v) other)
 
+-- parser is an alternetive -> used to apply the rules of the grammar and deal with the fact that the rule applied could be the wrong one automatically
 instance Alternative Parser where
     -- Parser a
     empty = P(\input->[])
@@ -57,65 +65,91 @@ instance Alternative Parser where
     -- Parser a -> Parser [a]
     many x = some x <|> pure []
 
+-- parser that extract the first character of a string and if it doesn't fail return a list with a couple with that char and the rest of the string
+-- e.g. parse item "ciao" -> [('c', "iao")]
+--      parse item "" -> []
 item :: Parser Char
 item = P(\input -> case input of 
                         [] -> []
                         (x:xs) -> [(x,xs)])
 
+-- function that given a predicate return a parser if the predicate is true
+-- e.g. parse (sat (=='c')) "ciao" -> [('c',"iao")]
+--      parse (sat (=='c')) "salve" -> []
 sat :: (Char -> Bool) -> Parser Char
 sat predicate = do x <- item
                    if (predicate x) 
                        then return x
                        else empty   
 
---check if the first character of a string is a digit
+-- check if the first character of a string is a digit
+-- e.g. parse (digit) "1234" -> [('1',"234")]
+--      parse (digit) "ciao" -> []
 digit :: Parser Char
 digit = sat isDigit
 
---check if the first character of a string is a lower case
+-- check if the first character of a string is a lower case
+-- e.g. parse (lower) "ciao" -> [('c',"iao")]
+--      parse (lower) "Ciao" -> []
 lower :: Parser Char
 lower = sat isLower
 
---check if the first character of a string is a upper case
-upper :: Parser Char
-upper = sat isUpper
-
---check if the first character of a string is a letter
+-- check if the first character of a string is a letter
+-- e.g. parse (lower) "ciao" -> [('c',"iao")]
+--      parse (lower) "1234" -> []
 letter :: Parser Char
 letter = sat isAlpha
 
---check if the first character of a string is an alphanumeric character
+-- check if the first character of a string is an alphanumeric character
+-- e.g. parse (alphanum) "ciao" -> [('c',"iao")]
+--      parse (alphanum) " ciao" -> []
 alphanum :: Parser Char
 alphanum = sat isAlphaNum
 
 --check if a string start with a certain character
+-- e.g. parse (char 'c') "ciao" -> [('c',"iao")]
+--      parse (char 'z') "ciao" -> []
 char :: Char -> Parser Char
 char c = sat (== c)
 
 --check if a string start with a certain substring
+-- e.g. parse (string "ci") "ciao" -> [("ci","ao")]
+--      parse (string "ma") "ciao" -> []
 string :: String -> Parser String
 string [] = return []
 string (c:cs) = do char c
                    string cs
                    return (c:cs)
 
---check if a string start with a lower case letter (if it is an identifier)
+-- check if a string could be an identifier (such as in the programming languages)
+-- e.g. parse identifier "c1Ao" -> [("c1Ao","")]
+--      parse identifier "Ciao" -> []
+--      parse identifier "2ciao" -> []
+--      parse identifier "ci_ao" -> [("ci","_ao")]
 ident :: Parser String
 ident = do x <- lower
            xs <- many alphanum
            return (x:xs)
 
---parse a natural number
+-- parse a natural number
+-- e.g. parse nat "88" -> [(88,"")]
+--      parse nat "-3" -> []
+--      parse nat "ciao" -> []
 nat :: Parser Int
 nat = do xs <- some digit
          return (read xs)
 
---remove one or more spaces
+-- remove one or more spaces before a string
+-- e.g. parse space "    ciao" -> [((),"ciao")]
+--      parse space "ciao" -> [((),"ciao")]
 space :: Parser ()
 space = do many (sat isSpace)
            return ()
 
---parse an int number: first it tries to read a '-' sign(negative integer), if it fails it tries to read a natural number(positive integer)
+-- parse an int number: first it tries to read a '-' sign(negative integer), if it fails it tries to read a natural number(positive integer)
+-- e.g. parse int "88" -> [(88,"")]
+--      parse int "-3" -> [(-3,"")]
+--      parse int "ciao" -> []
 int :: Parser Int
 int = do char '-'
          n <- nat
@@ -123,39 +157,45 @@ int = do char '-'
          <|>
          nat
 
---given a parser, remove the first spaces
+--given a parser, remove the spaces before and after the value
 token :: Parser a -> Parser a
 token parser = do space
                   value <- parser
                   space
                   return value
 
---parse a identifier
+-- check if a string could be an identifier (such as in the programming languages) removing spaces before and after
+-- e.g. parse identifier "   c1Ao   " -> [("c1Ao","")]
+--      parse identifier "Ciao" -> []
+--      parse identifier "2ciao" -> []
+--      parse identifier "ci_ao" -> [("ci","_ao")]
 identifier :: Parser String
 identifier = token ident
 
---parse a natural number
-natural :: Parser Int
-natural = token nat
-
---parse an integer
+-- parse an integer removing spaces before and after
+-- e.g. parse int "   88   " -> [(88,"")]
+--      parse int "   -3   " -> [(-3,"")]
+--      parse int "ciao" -> []
 integer :: Parser Int
 integer = token int
 
---parse a symbol
+-- recognize a given string removing spaces before and after
+-- e.g. parse (string "ci") "   ci   ao" -> [("ci","ao")]
+--      parse (string "ma") "ciao" -> []
 symbol :: String -> Parser String
 symbol xs = token (string xs)
 
---parse an array of integer
-nats :: Parser [Int]
-nats = do symbol "["
-          n <- natural
-          ns <- many (do symbol ","
-                         natural)
-          symbol "]"
-          return (n:ns)
 
---parse a sequence of variables
+-- *******************************************************************************************************************
+-- --------------------------------------------   LKC parsers   ------------------------------------------------------
+-- *******************************************************************************************************************
+
+
+-- parse a sequence of variables, separated by a comma
+-- e.g. parse parserSeqVar "x, y, z" -> [([VAR "x",VAR "y",VAR "z"],"")]
+--      parse parserSeqVar "x" -> [([VAR "x"),""]
+--      parse parserSeqVar "2x" -> []
+--      parse parserSeqVar "x," -> []
 parserSeqVar :: Parser [LKC]
 parserSeqVar = do x <- identifier
                   do symbol ","
@@ -166,7 +206,7 @@ parserSeqVar = do x <- identifier
                   <|>
                   return []
 
---parse an operation with prefix operator
+-- parse operations cons, eq and leq with prefix operator (operation with 2 parameters)
 parserOPP2 :: Parser (LKC->LKC->LKC)
 parserOPP2 = do symbol "cons"
                 return CONS
@@ -177,6 +217,7 @@ parserOPP2 = do symbol "cons"
              do symbol "leq"
                 return LEQ
 
+-- parse operations head and tail with prefix operator (operation with 1 parameters)
 parserOPP1 :: Parser (LKC->LKC)
 parserOPP1 = do symbol "head"
                 return H
@@ -185,7 +226,7 @@ parserOPP1 = do symbol "head"
                 return T
             
 
---parse a multiplicative operation with infix operator
+--parse a multiplicative operation (*,/) with infix operator
 parserOPM :: Parser (LKC -> LKC -> LKC)
 parserOPM = do symbol "*"
                return MULT
@@ -193,7 +234,7 @@ parserOPM = do symbol "*"
             do symbol "/"
                return DIV
 
---parse a aritmetic operation with infix operator
+--parse an aritmetic operation(+,-) with infix operator
 parserOPA :: Parser (LKC -> LKC -> LKC)
 parserOPA = do symbol "+"
                return ADD
@@ -201,6 +242,8 @@ parserOPA = do symbol "+"
             do symbol "-"
                return SUB
 
+-- parse a string that contains an aritmetic operation
+-- e.g. parse parserExpa "x+y" -> [(ADD (VAR "x") (VAR "y"),"")]
 parserExpa :: Parser LKC
 parserExpa = do term <- parserTerm
                 do opa <- parserOPA
@@ -209,6 +252,12 @@ parserExpa = do term <- parserTerm
                    <|>
                    return term
 
+-- parse a string that rapresent a factor (null, variables, function calls, integers or an in brackets aritmetic operation)
+-- e.g. parse parserFactor "null" -> [(NULL,"")]
+--      parse parserFactor "x" -> [(VAR "x","")]
+--      parse parserFactor "fun(2)" -> [(CALL (VAR "fun") [NUM 2],"")]
+--      parse parserFactor "-2" -> [(NUM (-2),"")]
+--      parse parserFactor "(x+y)" -> [(ADD (VAR "x") (VAR "y"),"")]
 parserFactor :: Parser LKC
 parserFactor = do symbol "null"
                   return (NULL)
@@ -219,7 +268,7 @@ parserFactor = do symbol "null"
                    <|>
                    return (VAR x))
                <|>
-               do num <- int
+               do num <- integer
                   return (NUM num)
                <|>
                do symbol "("
@@ -227,6 +276,8 @@ parserFactor = do symbol "null"
                   symbol ")"
                   return (expa)
 
+-- parse a string that rapresent a term (a factor or a multiplicative operation between terms)
+-- e.g. parse parserTerm "(x+y)*2" -> [(MULT (ADD (VAR "x") (VAR "y")) (NUM 2),"")]
 parserTerm :: Parser LKC
 parserTerm = do factor <- parserFactor
                 do opm <- parserOPM
@@ -235,62 +286,21 @@ parserTerm = do factor <- parserFactor
                    <|>
                    return (factor)
 
---parse a sequence of expressions
-parserSeqExpr :: Parser [LKC]
-parserSeqExpr = do expr <- parserExp
-                   do symbol ","
-                      exprs <- parserSeqExpr
-                      return (expr:exprs)
-                      <|>
-                      return ([expr])
-
---parse the parameters for a function
+--parse the parameters for a function if there is, otherwise it parse the empty brackets
+-- e.g. parse parserY "(x,y)" -> [([VAR "x",VAR "y"],"")]
+--      parse parserY "()" -> [([],"")]
 parserY :: Parser [LKC]
 parserY = do symbol "("
-             seq_expr <- parserSeqExpr
-             symbol ")"
-             return (seq_expr)
-          <|>
-          do symbol "("
-             symbol ")"
-             return []
+             do seq_expr <- parserSeqExpr
+                symbol ")"
+                return (seq_expr)
+                <|>
+                do symbol ")"
+                   return []
 
-parserExp :: Parser LKC
-parserExp = do symbol "lambda"
-               symbol "("
-               seq_var <- parserSeqVar
-               symbol ")"
-               expr <- parserExp
-               return (LAMBDA seq_var expr)
-            <|>
-            do opp2 <- parserOPP2
-               symbol "("
-               expr1 <- parserExp
-               symbol ","
-               expr2 <- parserExp
-               symbol ")"
-               return (opp2 expr1 expr2)
-            <|> 
-            do opp1 <- parserOPP1
-               symbol "("
-               expr1 <- parserExp
-               symbol ")"
-               return (opp1 expr1)
-            <|> 
-            do prog <- parserProg
-               return prog
-            <|>
-            do symbol "if"
-               test <- parserExp
-               symbol "then"
-               ifTrue <- parserExp
-               symbol "else"
-               ifFalse <- parserExp
-               return (IF test ifTrue ifFalse)
-            <|> 
-            do expa <- parserExpa
-               return expa
-
+-- parse dichiarations(one or more) of variables that are before the bosy of the program
+-- e.g. parse parserBind  "x = 7 and y = x and z = lambda (n) n*2" ->
+--          [([(VAR "x",NUM 7),(VAR "y",VAR "x"),(VAR "z",LAMBDA [VAR "n"] (MULT (VAR "n") (NUM 2)))],"")]
 parserBind :: Parser [(LKC, LKC)]
 parserBind = do x <- identifier
                 symbol "="
@@ -301,6 +311,9 @@ parserBind = do x <- identifier
                    <|>
                    return [(VAR x, expr)]
 
+-- parse an lkc program
+-- e.g. parse parserProg "let x=2 and y=4 in x+y*2 end" -> 
+--          [(LET (ADD (VAR "x") (MULT (VAR "y") (NUM 2))) [(VAR "x",NUM 2),(VAR "y",NUM 4)],"")]
 parserProg :: Parser LKC
 parserProg = do symbol "let"
                 bind <- parserBind
@@ -316,6 +329,61 @@ parserProg = do symbol "let"
                 symbol "end"
                 return (LETREC expr bind)
 
+-- parse an lkc expression
+parserExp :: Parser LKC
+parserExp = do symbol "lambda"
+               symbol "("
+               seq_var <- parserSeqVar
+               symbol ")"
+               expr <- parserExp
+               return (LAMBDA seq_var expr)
+               -- parse a lambda expression
+            <|>
+            do opp2 <- parserOPP2
+               symbol "("
+               expr1 <- parserExp
+               symbol ","
+               expr2 <- parserExp
+               symbol ")"
+               return (opp2 expr1 expr2)
+               -- parse a function with 2 parameters
+            <|> 
+            do opp1 <- parserOPP1
+               symbol "("
+               expr1 <- parserExp
+               symbol ")"
+               return (opp1 expr1)
+               -- parse a function with 1 parameter
+            <|> 
+            do prog <- parserProg
+               return prog
+               -- parse an lkc program
+            <|>
+            do symbol "if"
+               test <- parserExp
+               symbol "then"
+               ifTrue <- parserExp
+               symbol "else"
+               ifFalse <- parserExp
+               return (IF test ifTrue ifFalse)
+               -- parse an if-then-else construct
+            <|> 
+            do expa <- parserExpa
+               return expa
+               -- parse an aritmetic expression
+
+-- parse a sequence of expressions divided by a comma
+-- e.g. parse parserSeqExpr "x+y, x*y, fun(2), let x=2 in f(x) end" -> 
+--         [([ADD (VAR "x") (VAR "y"),MULT (VAR "x") (VAR "y"),CALL (VAR "fun") [NUM 2],LET (CALL (VAR "f") [VAR "x"]) [(VAR "x",NUM 2)]],"")]
+parserSeqExpr :: Parser [LKC]
+parserSeqExpr = do expr <- parserExp
+                   do symbol ","
+                      exprs <- parserSeqExpr
+                      return (expr:exprs)
+                      <|>
+                      return ([expr])
+
+-- evaluate a lkc expression. If it is correct return an lkc tree, if it is correct within a certain point gives an error and return the non parsed part of the expression, if it is wrong return a generic error
 eval :: String -> LKC
 eval s = case (parse parserExp s) of 
               [(lkc,[])] -> lkc
